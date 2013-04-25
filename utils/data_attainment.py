8bb6ecfcb2b9a0601d59723403fb05e464233ae2
Dominic May <jack.thatch@gmail.com>
2013-04-25 23:38:47 +0800

    Cleanup

--- utils/data_attainment.py
+++ utils/data_attainment.py
@@ -14,8 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
 "tumblr data accessing and processing"
+
 # stdlib
 import time
 import json
